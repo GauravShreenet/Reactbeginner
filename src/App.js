16e2473ca1172ee7802e379bdac39533d2ce5654
GauravShreenet <gthakuri656@gmail.com>
2023-09-19 14:36:17 +1000

    react beginner

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
 import './App.css';
-import Display from './Display.js';
-import { Heading } from './Display.js';
-import { Button } from './Button';
+// import Display from './Display.js';
+// import { Heading } from './Display.js';
+// import { Button } from './Button';
+
+// const App = () => {
+//   return (
+//     <div>
+//       <div>hellowww world</div>
+//       <div>hi</div>
+//       <hr />
+//       <Display />
+//       <Button />
+//       {/* <Paragraph /> */}
+//       <Heading />
+//     </div>
+//   );   
+  
+  
+// };
+
+// export default App;
 
 const App = () => {
+
+  const [user, setUser] = useState("");
+
+  const [userArg , setUserArg] = useState(["Gaurav", "Pream"])
+
+  // Controlled method
+  const handleOnChange = (e) => {
+    const { value } = e.target;
+    setUser(value);
+  }
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setUserArg([user, ...userArg])
+  }
+
   return (
     <div>
-      <div>hellowww world</div>
-      <div>hi</div>
-      <hr />
-      <Display />
-      <Button />
-      {/* <Paragraph /> */}
-      <Heading />
+      <form action="" className='user-form'>
+        <p>{user}</p>
+        <input onChange={handleOnChange} type="text" />
+        <button onClick={handleOnSubmit}>Add User</button>
+      </form>
+
+      <div>
+        <ul>
+          {
+            userArg.map((item, i)=><li>{item}</li>)
+          }
+        </ul>
+      </div>
     </div>
-  );   
-  
-  
-};
+  )
+}
 
 export default App;
